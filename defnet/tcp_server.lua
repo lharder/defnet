6239d2e9a0421bde5bc24a68f78365ab48cb190d
lutz-harder <lutz.harder@freenet.ag>
2019-12-31 13:03:19 +0100

    test

--- defnet/tcp_server.lua
+++ defnet/tcp_server.lua
@@ -154,7 +154,7 @@ function M.create(port, on_data, on_client_connected, on_client_disconnected)
 			coroutine.wrap(function()
 				local data, err = server.receive(client)
 				if data and on_data then
-					local client_ip, client_port = client:getsockname()
+					local client_ip, client_port = client:getpeername()
 					local response = on_data(data, client_ip, client_port, client, function(response)
 						if not queues[client] then
 							return false
